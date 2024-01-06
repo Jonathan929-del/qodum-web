bae8fc96aea1d832fbdd50cf360d0a16a7524e97
Jonathan <80016156+Jonathan929-del@users.noreply.github.com>
2024-01-06 21:19:41 +0200

    Created vehicle route page

---
+++ app/(root)/fees/(transport)/define-vehicle-route/page.tsx
@@ -0,0 +1,73 @@
+'use client';
+// Imports
+import {useEffect, useState} from 'react';
+import FormCom from '@/components/modules/fees/transport/vehicleRoute/FormCom';
+import ViewCom from '@/components/modules/fees/transport/vehicleRoute/ViewCom';
+import {fetchVehicleRoutes} from '@/lib/actions/fees/transport/vehicleRoute.actions';
+
+
+
+
+
+// Main function
+const page = () => {
+
+
+    // Is view component opened
+    const [isViewOpened, setIsViewOpened] = useState(false);
+
+
+    // Vehicle routes
+    const [vehicleRoutes, setVehicleRoutes] = useState([{}]);
+
+
+    // Update vehicle route
+    const [updateVehicleRoute, setUpdateVehicleRoute] = useState({
+        id:'',
+        isDeleteClicked:false,
+        route_no:'',
+        route_description:'',
+        route_in_charge_name:'',
+        route_in_charge_mobile_no:''
+    });
+
+
+    // Use effect
+    useEffect(() => {
+        const vehicleRoutesFetcher = async () => {
+            const res = await fetchVehicleRoutes();
+            setVehicleRoutes(res);
+        };
+        vehicleRoutesFetcher();
+    }, [isViewOpened, updateVehicleRoute]);
+
+
+    return (
+        <div className='h-screen flex flex-col items-center justify-start pt-10 bg-white overflow-hidden'>
+            {
+                isViewOpened ? (
+                    <ViewCom
+                        setIsViewOpened={setIsViewOpened}
+                        vehicleRoutes={vehicleRoutes}
+                        setUpdateVehicleRoute={setUpdateVehicleRoute}
+                    />
+                ) : (
+                    <FormCom
+                        isViewOpened={isViewOpened}
+                        setIsViewOpened={setIsViewOpened}
+                        vehicleRoutes={vehicleRoutes}
+                        updateVehicleRoute={updateVehicleRoute}
+                        setUpdateVehicleRoute={setUpdateVehicleRoute}
+                    />
+                )
+            }
+        </div>
+    );
+};
+
+
+
+
+
+// Export
+export default page;
