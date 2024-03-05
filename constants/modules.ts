544794ac9e57c98de98ab90f147371682e1ac7b8
Jonathan <80016156+Jonathan929-del@users.noreply.github.com>
2024-03-05 05:28:49 +0200

    Fixed issues 5

--- constants/modules.ts
+++ constants/modules.ts
@@ -8,7 +8,7 @@ import UsersImage from '@/public/assets/Modules Icons/Users.png';
 import AttendenceImage from '@/public/assets/Modules Icons/Attendance.png';
 import TimetableImage from '@/public/assets/Modules Icons/Users.png';
 import ExamanationImage from '@/public/assets/Modules Icons/Examination.png';
-import LibraryImage from '@/public/assets/Modules Icons/LIBRARY.png';
+import LibraryImage from '@/public/assets/Modules Icons/Library.png';
 import CBSEImage from '@/public/assets/Modules Icons/CBSE DIOS.png';
 
 
