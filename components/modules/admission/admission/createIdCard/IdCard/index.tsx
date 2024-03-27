664a0ddca93b1010e9637b7d8d34b2c9b3c7a383
Jonathan <80016156+Jonathan929-del@users.noreply.github.com>
2024-03-27 15:24:19 +0200

    Fixed card issues 2

--- components/modules/admission/admission/createIdCard/IdCard/index.tsx
+++ components/modules/admission/admission/createIdCard/IdCard/index.tsx
@@ -2,6 +2,7 @@
 // Imports
 import {X} from 'lucide-react';
 import PdfView from './PdfView';
+import LoadingIcon from '@/components/utils/LoadingIcon';
 
 
 
@@ -10,7 +11,13 @@ import PdfView from './PdfView';
 // Main function
 const index = ({setIsCardOpened, studentData}:any) => {
     return (
-        <div className='w-[90%] h-[90%] flex flex-col items-center pb-2 gap-2 rounded-[8px] border-[0.5px] border-[#E8E8E8]'>
+        <div className='relative w-[90%] h-[90%] flex flex-col items-center pb-2 gap-2 rounded-[8px] border-[0.5px] border-[#E8E8E8]'>
+
+        {studentData.school_name === '' && (
+            <div className='absolute w-full h-full top-0 left-0 bg-[#fff] flex items-center justify-center'>
+                <LoadingIcon />
+            </div>
+        )}
 
         {/* Header */}
         <div className='flex flex-row items-center justify-between w-full px-2 py-2 text-sm font-bold text-main-color bg-[#e7f0f7] rounded-t-[8px]'>
