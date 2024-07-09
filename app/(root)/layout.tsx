b27e6c13de64a91fda1c6aba6e4a8057714dc71f
Jonathan <80016156+Jonathan929-del@users.noreply.github.com>
2024-07-09 21:10:39 +0300

    Fixed issues 49

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,7 +2,7 @@
 import '../globals.css';
 import type {Metadata} from 'next';
 import {Inter} from 'next/font/google';
-import {ClerkProvider} from '@clerk/nextjs';
+// import {ClerkProvider} from '@clerk/nextjs';
 import Layout from '@/components/Layout/index';
 // import {GlobalStateProvider} from '@/context/GlobalStateContext';
 
@@ -26,11 +26,11 @@ export default function RootLayout({children}:{children:React.ReactNode}) {
   return (
     <html lang="en">
       <body className={`${inter.className}`}>
-          <ClerkProvider>
+          {/* <ClerkProvider> */}
             {/* <GlobalStateProvider> */}
               <Layout children={children} />
             {/* </GlobalStateProvider> */}
-          </ClerkProvider>
+          {/* </ClerkProvider> */}
       </body>
     </html>
   );
