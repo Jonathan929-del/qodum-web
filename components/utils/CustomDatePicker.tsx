87319ed74c5baaf6fc4f1c0ffffc5be84b9bdb0e
Jonathan <80016156+Jonathan929-del@users.noreply.github.com>
2024-07-11 08:03:31 +0300

    Added date picker library

--- components/utils/CustomDatePicker.tsx
+++ components/utils/CustomDatePicker.tsx
@@ -2,7 +2,7 @@
 import moment from 'moment';
 import 'moment/locale/en-gb'; 
 import './CustomDatePickerStyles.css';
-// import DatePicker from 'react-datepicker2';
+import DatePicker from 'react-datepicker2';
 
 
 
@@ -15,14 +15,13 @@ const MyDatePicker = ({selectedDate, setSelectedDate}) => {
   moment.locale('en-gb');
 
   return (
-    // <DatePicker
-    //   value={selectedDate}
-    //   onChange={(date) => setSelectedDate(date)}
-    //   timePicker={false}
-    //   // @ts-ignore
-    //   dateFormat="MMMM YYYY"
-    // />
-    <div>Date</div>
+    <DatePicker
+      value={selectedDate}
+      onChange={(date) => setSelectedDate(date)}
+      timePicker={false}
+      // @ts-ignore
+      dateFormat="MMMM YYYY"
+    />
   );
 };
 
