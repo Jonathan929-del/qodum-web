0ea70f49b6e1568d5f510447551b71b8d71f815d
Jonathan <80016156+Jonathan929-del@users.noreply.github.com>
2024-09-06 14:16:04 +0300

    Fixed enquiry no setting validation

--- lib/validations/admission/masterSettings/EnquiryNoSetting.validation.ts
+++ lib/validations/admission/masterSettings/EnquiryNoSetting.validation.ts
@@ -12,5 +12,5 @@ export const EnquiryNoSettingValidation = z.object({
     prefix:z.string().nonempty({message:'*Please enter prefix'}),
     start_from:z.number({invalid_type_error:'*Please enter start from value'}).or(z.string().nonempty({message:'*Please enter start from value'})).pipe(z.coerce.number({invalid_type_error:'*Please enter a numeric value'})),
     lead_zero:z.string().nonempty({message:'*Please enter lead zero value'}),
-    suffix:z.string().nonempty({message:'*Please enter suffix'})
+    suffix:z.string()
 });
