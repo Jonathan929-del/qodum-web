36dff9f96cf9ceebdf5fdebc92b5615cccc0da7b
Jonathan <80016156+Jonathan929-del@users.noreply.github.com>
2024-10-02 14:18:11 +0300

    Fixed login issue 16

--- lib/actions/users/manageUsers/user.actions.ts
+++ lib/actions/users/manageUsers/user.actions.ts
@@ -845,15 +845,20 @@ export const loginUser = async ({user_name, password}:any) => {
 
 
         // loging user
-        const token = signToken(JSON.parse(JSON.stringify(user)));
+        // const token = signToken(JSON.parse(JSON.stringify(user)));
         return({
             success:true,
             user:JSON.parse(JSON.stringify({
                 ...user._doc,
-                token
             }))
         });
-        // return JSON.parse(JSON.stringify(user));
+        // return({
+        //     success:true,
+        //     user:JSON.parse(JSON.stringify({
+        //         ...user._doc,
+        //         token
+        //     }))
+        // });
 
     }catch(err){
         throw new Error(`Error with user login: ${err}`);  
